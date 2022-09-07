f8df82e0127f8d36bc3d1df72c30ddfa8885fb6c
AquaApple <yassershehab9@gmail.com>
2022-09-07 23:59:20 +0200

    Added Styles for mainHeader

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
+import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/welcome">Welcome</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/welcome">Welcome</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
