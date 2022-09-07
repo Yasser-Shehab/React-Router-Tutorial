6686a680aa579a928a1d3d3da5b02b5c74fcceb0
AquaApple <yassershehab9@gmail.com>
2022-09-08 00:12:46 +0200

    Added Dynamic routes using UseParams hook

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,14 @@
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>Book</li>
+        <li>Video Game</li>
+        <li>Chess</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
